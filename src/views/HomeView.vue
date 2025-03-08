53fd5e7176f6acccf1ca06ef90b7315f8d9081e5
Jesus Quinones <jesuga27@gmail.com>
2025-03-08 17:19:42 -0400

    realizando fetching de los datos de los personajes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { ref } from 'vue'
+
+const characters = ref([])
+
+const fetchData = async () => {
+  const res = await fetch('https://rickandmortyapi.com/api/character')
+  const data = await res.json()
+  characters.value = data.results
+  console.log(characters)
+}
+
+onMounted(() => {
+  fetchData()
+})
+</script>
 
 <template>
-  <main>Vue JS</main>
+  <main>
+    <header>
+      <h1 class="text-center">Rick and Morty Api</h1>
+    </header>
+
+    <section class="flex flex-wrap justify-center gap-8">
+      <article v-for="character in characters" :key="character.id">
+        <img class="rounded-lg" :src="character.image" :alt="character.name" />
+      </article>
+    </section>
+  </main>
 </template>

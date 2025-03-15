1273ed38fce9bd743c489f4061d92ad531347013
Jesus Quinones <jesuga27@gmail.com>
2025-03-15 15:48:56 -0400

    funciones para cambiar de paginas listas

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,16 +4,28 @@ import { ref } from 'vue'
 import type { Character } from '../types/Character.ts'
 
 const characters = ref<Character[]>([])
+const page = ref<number>(1)
 
-const fetchData = async () => {
-  const res = await fetch('https://rickandmortyapi.com/api/character')
+const loadCharacters = async () => {
+  const res = await fetch(`https://rickandmortyapi.com/api/character/?page=${page.value}`)
   const data = await res.json()
   characters.value = data.results
   console.log(data.results)
 }
 
+const nextPage = () => {
+  page.value++
+  loadCharacters()
+}
+
+const prevPage = () => {
+  if (page.value === 1) return
+  page.value--
+  loadCharacters()
+}
+
 onMounted(() => {
-  fetchData()
+  loadCharacters()
 })
 </script>
 
@@ -25,6 +37,7 @@ onMounted(() => {
 
     <section id="pagination">
       <button
+        @click="prevPage"
         class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
         {{ '<-' }}
@@ -32,9 +45,10 @@ onMounted(() => {
       <span
         class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
       >
-        {{ '20' }}
+        {{ page }}
       </span>
       <button
+        @click="nextPage"
         class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
         {{ '->' }}
